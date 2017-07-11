fe4e03b8d308d8614b609342d3ba6bdf8ec68478
Beda Tse <beda@ananse.io>
2017-07-12 01:00:50 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# antplus-link
+Listen to ANT+ device broadcasts and publish to via a pub/sub protocol
---
+++ gh-pages/raspbian-jessie-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+echo '===== Installing NodeJS and Yarn ====='
+curl -sL https://deb.nodesource.com/setup_5.x | sudo bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+apt-get -y update
+apt-get -y install nodejs yarn build_essentials
+echo '===== Installing nodeJS support tools ====='
+echo "NodeJS Version: `node -v`"
+echo "NPM Version: `npm -v`"
+echo "Yarn Version: `yarn --version`"
+npm install -g typescript bunyan
